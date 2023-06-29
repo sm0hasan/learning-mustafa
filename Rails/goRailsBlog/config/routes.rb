ab46615114033b8fa35d99ca8e226846f258cc08
Syed Mustafa Hasan <1hasanmus2@gmail.com>
2023-06-29 14:22:22 -0400

    Semi-complete, need to add comment structure later

--- Rails/goRailsBlog/config/routes.rb
+++ Rails/goRailsBlog/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [:destroy], module: :blog_posts
+  end
 
   # The below will be replaced with resources in which Rails configures the routes for us
     # get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
